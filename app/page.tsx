07c7ba13f70f1ebedf89f2e7d62e31a34b0e990f
HIROKI UCHIYAMA <nextgamecoo@gmail.com>
2026-04-05 20:56:12 +0900

    page.tsx を更新

--- app/page.tsx
+++ app/page.tsx
@@ -632,7 +632,7 @@ export default async function HomePage() {
             </div>
             <div className="aspect-square overflow-hidden border border-black/10">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d812.9!2d136.91404!3d35.16868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60037a0fee0c8b8b%3A0xe2b8e9f4a59dcbdd!2z5Lqs6YO96Zmc56iy44Gq44GK44GN!5e0!3m2!1sja!2sjp!4v1742000000000"
+                src="https://maps.google.com/maps?q=京都焼肉なおき+愛知県名古屋市中区栄4-6-18&output=embed&hl=ja"
                 width="100%"
                 height="100%"
                 loading="lazy"
